a16b8177a2c8279a62207a9aec8fa8568653d2d0
Rachanacs <70090917+rachana-c-s@users.noreply.github.com>
2021-07-20 13:29:58 +0530

    emoji.updated

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ export default function App() {
 
   return (
     <div className="App">
-      <h1>inside outtt!</h1>
+      <h1>Emoji Interpreter🎁</h1>
 
       <input onChange={emojiInputHandler} />
 
